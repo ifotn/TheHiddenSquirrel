0405a252ba4af4464e292cd8986799a12752fea6
ifotn <Rich.Freeman@GeorgianCollege.ca>
2025-09-10 09:46:58 -0400

    Add project files.

---
+++ TheHiddenSquirrel/Controllers/CategoriesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TheHiddenSquirrel.Models;
+
+namespace TheHiddenSquirrel.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            // use Category model to create list of in-memory categories (replaced by db soon)
+            var categories = new List<Category>();
+
+            for (var i = 1; i < 11; i++)
+            {
+                categories.Add(new Category { CategoryId = i, Name = "Category " + i.ToString() });
+            }
+
+            // pass categories list for display when loading the view
+            return View(categories);
+        }
+    }
+}
---
+++ TheHiddenSquirrel/Models/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace TheHiddenSquirrel.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+    }
+}
