35dc8633dabfc1f5eb754f605b8a75dc9f08759c
ifotn <Rich.Freeman@GeorgianCollege.ca>
2025-09-24 10:26:29 -0400

    l4/1 - migration, create & read categories

--- TheHiddenSquirrel/Controllers/CategoriesController.cs
+++ TheHiddenSquirrel/Controllers/CategoriesController.cs
@@ -1,19 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TheHiddenSquirrel.Data;
 using TheHiddenSquirrel.Models;
 
 namespace TheHiddenSquirrel.Controllers
 {
     public class CategoriesController : Controller
     {
+        // shared db connection obj
+        private readonly ApplicationDbContext _context;
+
+        // constructor with connection dependency
+        public CategoriesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             // use Category model to create list of in-memory categories (replaced by db soon)
-            var categories = new List<Category>();
+            //var categories = new List<Category>();
 
-            for (var i = 1; i < 11; i++)
-            {
-                categories.Add(new Category { CategoryId = i, Name = "Category " + i.ToString() });
-            }
+            //for (var i = 1; i < 11; i++)
+            //{
+            //    categories.Add(new Category { CategoryId = i, Name = "Category " + i.ToString() });
+            //}
+
+            // use DbSet to fetch list of categories from db
+            var categories = _context.Category.ToList();
 
             // pass categories list for display when loading the view
             return View(categories);
@@ -23,6 +36,24 @@ namespace TheHiddenSquirrel.Controllers
         {
             // display blank form to add a new category
             return View();
+        }
+
+        // POST: /Categories/Create => process form submission to save new Category
+        [HttpPost]
+        public IActionResult Create([Bind("Name")] Category category)
+        {
+            // validate category first
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            // save to db
+            _context.Category.Add(category);
+            _context.SaveChanges();
+
+            // redirect to Index page
+            return RedirectToAction("Index");
         }
 
         public IActionResult Edit()
--- TheHiddenSquirrel/Data/Migrations/20250924140316_CreateCategoriesAndProducts.Designer.cs
+++ TheHiddenSquirrel/Data/Migrations/20250924140316_CreateCategoriesAndProducts.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TheHiddenSquirrel.Data;
 
@@ -11,9 +12,11 @@ using TheHiddenSquirrel.Data;
 namespace TheHiddenSquirrel.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250924140316_CreateCategoriesAndProducts")]
+    partial class CreateCategoriesAndProducts
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
