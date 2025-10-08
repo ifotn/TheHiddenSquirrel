c8fc307668ba98be00a5981d5b0303632d2a3e20
ifotn <Rich.Freeman@GeorgianCollege.ca>
2025-10-08 09:50:33 -0400

    l6/3 - auto-assign every new user to Customer role

--- TheHiddenSquirrel/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ TheHiddenSquirrel/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -122,6 +122,9 @@ namespace TheHiddenSquirrel.Areas.Identity.Pages.Account
                 {
                     _logger.LogInformation("User created a new account with password.");
 
+                    // automatically assign every new user to the role of Customer
+                    await _userManager.AddToRoleAsync(user, "Customer");
+
                     var userId = await _userManager.GetUserIdAsync(user);
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
                     code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
