cc13d92c88923fa26e39911a8b0ccff9f88d129d
ifotn <Rich.Freeman@GeorgianCollege.ca>
2025-11-05 08:38:36 -0500

    l9/1 - Shop Index

--- TheHiddenSquirrel/Controllers/ShopController.cs
+++ TheHiddenSquirrel/Controllers/ShopController.cs
@@ -1,12 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TheHiddenSquirrel.Data;
 
 namespace TheHiddenSquirrel.Controllers
 {
     public class ShopController : Controller
     {
+        // shared db connection
+        private readonly ApplicationDbContext _context;
+
+        // constructor to initialize db connection
+        public ShopController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: /Shop/Index => show Categories as cards so user can select one
         public IActionResult Index()
         {
-            return View();
+            // fetch categories
+            var categories = _context.Category.OrderBy(c => c.Name).ToList();
+
+            // show view and pass categories list
+            return View(categories);
         }
     }
 }
