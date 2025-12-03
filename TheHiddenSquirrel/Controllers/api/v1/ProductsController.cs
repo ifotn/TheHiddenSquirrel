845e975665c6b9f15a66a9c65d6956f31e513833
ifotn <Rich.Freeman@GeorgianCollege.ca>
2025-12-03 09:44:48 -0500

    l13/1 - GET api methods

---
+++ TheHiddenSquirrel/Controllers/api/v1/ProductsController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TheHiddenSquirrel.Data;
+
+namespace TheHiddenSquirrel.Controllers.api.v1
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        // shared db conn for all CRUD methods
+        private readonly ApplicationDbContext _context;
+
+        // constructor w/db dependency
+        public ProductsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: /api/v1/products => show all products
+        [HttpGet]
+        public IActionResult Index()
+        {
+            // return all products as json
+            var products = _context.Product.ToList();
+
+            return Ok(products);
+        }
+
+        // GET: /api/v1/products/28 => show selected product
+        [HttpGet("{id}")]
+        public IActionResult Details(int id)
+        {
+            // get selected product
+            var product = _context.Product.Find(id);
+
+            // if invalid id => 404
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+
+        //// POST: /api/v1/products => insert new product from request body
+        //[HttpPost]
+        //public IActionResult Create(Product product)
+        //{
+
+        //}
+
+    }
+}
